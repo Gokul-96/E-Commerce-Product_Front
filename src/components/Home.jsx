b4603a0fc6080d4b79896c66cf34629bd1233c20
gokul <gokulakrishnan.m96@gmail.com>
2025-01-25 07:56:47 +0530

    Deployed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
     <div>
       
       <main className="container mx-auto mt-6">
+        <h1>Login Credential</h1>
+        <p>Email: binu@gmail.com</p>
+        <p>Password: binu123</p>
         <h2 className="text-xl font-bold">Featured Products</h2>
         <ProductList /> 
       </main>
